f7249fbbc91c9ed2833cb9f5b52a965cab18e2d6
Benjaminatedu <91627659+Benjaminatedu@users.noreply.github.com>
2021-10-06 12:12:27 -0600

    Added Director and cardDeal classes

---
+++ Director.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace cse210_tc04
+{
+    class Director
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ cardDeal.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace cse210_tc04
+{
+    class Card
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
